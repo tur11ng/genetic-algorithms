172623c695675035896263ac74ee32375614c089
st1064870 <st1064870@ceid.upatras.gr>
2020-06-09 19:02:11 +0300

    Fix bug

--- src/helpers.py
+++ src/helpers.py
@@ -38,6 +38,7 @@ def get_nearest_neighbors(individuals: np.ndarray, individual: int, n_neighbors:
         distances.append(metric(individual, individuals[i]))
 
     sorted_distances_ind = np.argsort(np.array(distances))[-11:-1]
+    sorted_distances_ind = sorted_distances_ind[::-1]
     return individuals[sorted_distances_ind]
 
 
