8443c156b60c3aac2ec12877ba4213cf22ec2646
st1064870 <st1064870@ceid.upatras.gr>
2020-06-15 00:38:37 +0300

    Add support for hold out

--- src/parser.py
+++ src/parser.py
@@ -1,16 +1,29 @@
+import numpy as np
 import pandas as pd
 
 
-def parse_ml100k():
+def parse_ml100k(filename):
     # Read data
-    ds = pd.read_csv('ml-100k/u.data', sep='\t',
+    df = pd.read_csv('ml-100k/' + filename, sep='\t',
                      names=['user_id', 'movie_id', 'rating', 'unix_timestamp'], encoding='latin-1')
 
     # Dropping the columns that are not required
-    ds.drop("unix_timestamp", inplace=True, axis=1)
+    df.drop("unix_timestamp", inplace=True, axis=1)
 
     # Generate matrix
-    ds = ds.pivot_table(index=['user_id', ], columns=['movie_id'],
+    df = df.pivot_table(index=['user_id', ], columns=['movie_id'],
                         values='rating').reset_index(drop=True)
+    return df
 
-    return ds.to_numpy()
+
+def prepare_holdout():
+    df = parse_ml100k('u.data')
+    df_train = parse_ml100k('ua.base')
+    df_test = parse_ml100k('ua.test')
+
+    empty_df = pd.DataFrame(np.nan, index=df.index, columns=df.columns)
+
+    df_test = empty_df.merge(right=df_test, how='right')
+    df_train = empty_df.merge(right=df_train, how='right')
+
+    return df_train, df_test
